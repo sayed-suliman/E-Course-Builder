78237ee79923bd311d2c009fd5664c465f513fde
sayed <sulimansayed003@gmail.com>
2022-09-02 18:27:15 +0500

    dashboard page and login page with the first commit

---
+++ Assets/Js/index.js
@@ -0,0 +1,26 @@
+console.log("Your js file is working now successfully..!!")
+let bool = true
+function openNav() {
+    console.log(bool)
+    if (bool) {
+        document.getElementById("mySidebar").style.width = "250px";
+        bool = false
+    }
+     else {
+        document.getElementById("mySidebar").style.width = "20px";
+        bool = true
+    }
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "20px";
+    bool = true
+}
+// document.body.addEventListener('click', function(e){
+//     if(e.target.className != "sidebar" && e.target.parentNode.className != "sidebar"){
+//         document.getElementById("mySidebar").style.width = "20px";
+//         bool = true
+//         console.log("hekki")
+//     }
+//     console.log(e.target.parentNode.className)
+//     console.log(e.target)
+// }, true); 
