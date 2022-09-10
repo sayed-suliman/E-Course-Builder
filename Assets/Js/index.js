7b94258b8efbddee99282f74d410b44de02ce674
sayed <sulimansayed003@gmail.com>
2022-09-10 14:44:16 +0500

    create whitebaord page is added

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -1,4 +1,12 @@
 console.log("Your js file is working now successfully..!!")
+// tablet screen
+var tabletScreen = window.matchMedia("(max-width:1024px)")
+// mobile screen
+var mobileScreen = window.matchMedia("(max-width:425px)")
+
+
+
+
 let bool = true
 function openNav() {
     console.log(bool)
@@ -15,16 +23,65 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "24px";
     bool = true
 }
-// document.body.addEventListener('click', function(e){
-//     if(e.target.className != "sidebar" && e.target.parentNode.className != "sidebar"){
-//         document.getElementById("mySidebar").style.width = "20px";
-//         bool = true
-//         console.log("hekki")
-//     }
-//     console.log(e.target.parentNode.className)
-//     console.log(e.target)
-// }, true); 
 
 // enabling tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// *********************
+
+    // Creating whitebaord page
+
+// ***********************************
+
+let switchHideShow = document.querySelector("#switch input")
+let navMenu = document.querySelector(".side-menu .nav-menu")
+switchHideShow.addEventListener("change",()=>{
+    if((switchHideShow.checked)){
+        tabletScreenFtnHideOrOpen(tabletScreen)
+        navMenu.style.transform = "translate(-150px,0)"
+        navMenu.style.opacity = "0"
+    }else{
+        navMenu.style.transform = "translate(0,0)"
+        navMenu.style.opacity = "1"
+        testyFunction(tabletScreen)
+    }
+    // for tablet screen
+})
+// 
+function tabletScreenFtnHideOrOpen(x){
+    if((x.matchMedia || window.innerWidth <= 1024) && switchHideShow.checked){
+        navMenu.style.visibility = "hidden"
+        navMenu.style.opacity = "0"
+        navMenu.style.transform = "translate(-150px,0)"
+
+    }
+    if((window.innerWidth > 1024) && !(switchHideShow.checked)){
+        navMenu.style.visibility = "visible"
+        navMenu.style.transform = "translate(0,0)"
+        navMenu.style.opacity = "1"
+    }
+    
+}
+function testyFunction(x){
+        navMenu.style.transform = "translate(0,0)"
+        navMenu.style.visibility = "visible"
+        navMenu.style.opacity = "1"
+}
+// for in mobile screen the nav should be visible 
+function sliderOn(x){
+    console.log("asd")
+    if(x.matches || window.innerHeight <= 425){
+        console.log("fff")
+        navMenu.style.transform = "translate(0,0)"
+        navMenu.style.visibility = "visible"
+    }
+}
+tabletScreen.addListener(tabletScreenFtnHideOrOpen)
+
+
+// for mobile screen nav should be visible
+console.log(tabletScreen)
+sliderOn(mobileScreen)
+mobileScreen.addListener(sliderOn)
+// tabletScreen.addListener(testyFunction)
